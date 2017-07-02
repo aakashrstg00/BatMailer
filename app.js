e418bba4e5e85df3c555569cefccd49098fe1751
Aakash Rastogi <1998aakashrastogi@gmail.com>
2017-07-03 01:18:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var nodemailer = require("nodemailer");
+var app = express();
+app.use(express.static('public'));
+//
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+//
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
+app.get('/send', function (req, res) {
+    //code to send e-mail.
+    var smtpTransport = nodemailer.createTransport({
+        service: "gmail"
+        , host: "smtp.gmail.com"
+        , secuse: true
+        , port: 465
+        , auth: {
+            user: 'aakashrastogiPro@gmail.com'
+            , pass: '#Aakash1998g2'
+        }
+    });
+    var mailOptions = {
+        from: '"Aakash3 Rastogi 👻" <aakashrastogiPro@gmail.com>'
+        , to: req.query.to
+        , subject: req.query.subject
+        , text: req.query.text
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function (error, response) {
+        if (error) {
+            console.log(error);
+            res.end("error");
+        }
+        else {
+            console.log("Message sent to " + req.query.to);
+            res.end("sent");
+        }
+    });
+});
+//
+app.listen(3000, function () {
+    console.log("Express Started on Port 3000");
+});
